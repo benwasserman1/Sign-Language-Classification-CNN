7df566b92baa57dc5dc0587825837238761d5ec9
Ben Wasserman <wasse114@mail.chapman.edu>
2019-01-23 21:04:29 -0800

    augmented data

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 """
 Created on Mon Jan 21 22:14:13 2019
 
-@author: benjaminwasserman
+@author: benjaminwasserman and ryanturley
+
+Ben Wasserman and Ryan Turley
+CPSC393 Machine Learning
+January 24th, 2018
 """
 
+# Import libraries: matplotlib for showing images, sklearn for splitting, numpy for arrays, keras for CNN
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import Sequential
@@ -14,26 +19,22 @@ from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 
 
-datagen = ImageDataGenerator(
-    rotation_range=16,
-    width_shift_range=0.08,
+# Generate augmented data so the results will generalize better 
+aug_data = ImageDataGenerator(
+    rotation_range=3,
+    width_shift_range=0.04,
     height_shift_range=0.08,
-    zoom_range=.08,
-    shear_range = .08)
-
+    zoom_range=.12,
+    shear_range = .1)
 
 # EDA: show the first image, show the shape, split into training and test, 
-# find length of eac3h
-
+# and find length of each so x_train and x_test can be reshaped properly
 x = np.load("Sign-language-digits-dataset 2/X.npy")
 y = np.load("Sign-language-digits-dataset 2/Y.npy")
 
 
 # plot the first image in the dataset
 plt.imshow(x[0], cmap = "gray")
-
-# check the image shape
-#print(x[0].shape)
 
 # split into test and training data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.15)
@@ -42,46 +43,79 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.15)
 x_train_count = len(x_train)
 x_test_count = len(x_test)
 
-
 # get full shape
 print(x_train.shape)
 print(x_train.shape[1:])
-
 
 # reshape data to fit model
 x_train = x_train.reshape(x_train_count, 64, 64, 1)
 x_test = x_test.reshape(x_test_count, 64, 64, 1)
 
-
-# create model
+# create the model
 model = Sequential()
 
-#add model layers
+# add two convolutional layers and max pool
 model.add(Conv2D(64, kernel_size=4, activation='relu', input_shape=(64,64, 1)))
 model.add(Conv2D(64, kernel_size=4, activation='relu'))
 model.add(MaxPooling2D(pool_size=4))
+model.add(Conv2D(64, kernel_size=4, activation='relu'))
+model.add(Conv2D(64, kernel_size=4, activation='relu'))
 
-model.add(Dropout(0.3))
+# dropout to help with overiftting
+model.add(Dropout(0.35))
 model.add(Flatten())
 
+# pass through fully connected layer
 model.add(Dense(units = 256, activation = 'relu'))
+
+# use BatchNormalization to minimize how layers affect each other
 model.add(BatchNormalization())
+
+# softmax for output layer
 model.add(Dense(10, activation='softmax'))
 
-#compile model using accuracy to measure model performance
+#compile model
 model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
 
-#train the model
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=8)
+# train the model with the augmented data from datagen
+model.fit_generator(aug_data.flow(x_train, y_train, batch_size=64),
+                    steps_per_epoch=64, epochs=7)
 
-model.fit_generator(datagen.flow(x_train, y_train, batch_size=32),
-                    steps_per_epoch=64, epochs=8)
 
+# show validation accuracy of model
 score = model.evaluate(x_test, y_test, verbose=0)
+print('Validation loss: {:.4f}  Validation accuracy: {:.4}%'.format(score[0],score[1]))
 
-print('Loss: {:.4f}  Accuaracy: {:.4}%'.format(score[0],score[1]))
 
-model.predict(x_test[0:])
+# get results array and convert it to a list
+results = model.predict(x_test[0:])
+results = results.tolist()
+
+# convert actual labels to a list
 y_test[0:]
+encodings = y_test[0:].tolist()
+
+predicted_labels = []
+proper_labels = []
+
+# create a list of the predicted and proper values for each image
+for encoding in encodings:
+    value = max(encoding)
+    proper_labels.append(encoding.index(value))
+
+for result in results:
+    value = (max(result))
+    predicted_labels.append(result.index(value))
+    
+print(proper_labels)
+print(predicted_labels)
+
+
+# print out what was predicted improperly 
+for i in range(len(proper_labels)):
+    if proper_labels[i] != predicted_labels[i]:
+        print("Mistook " + str(predicted_labels[i]) + " for " + str(proper_labels[i]))
+        
+        
 
 
